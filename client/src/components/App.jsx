90fd25ef3422b96de1ad775a7150d0375f664fb3
Takudzwa Karigomba <takkarigomba@gmail.com>
2020-03-03 18:28:13 +0000

    nav bar added up top

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,16 +4,20 @@ import Songs   from './Songs';
 import Song    from './Song';
 import AddSong from './AddSong';
 import Header from './Header';
+import Nvrbar       from './Nvrbar';
 class App extends React.Component {
 
   render() {
     return (
+      <div>
+      <Nvrbar />
       <Router>
         <Songs   path='/' />
         <Song    path='/song/:songID' />
         <AddSong path='/add-song/' />
-        <Header  path ='/Header'/>
+        <Header  path ='./Header'/>
       </Router>
+      </div>
     );
   }
 
